f14a750d1b70915d5329b2ea67fc4bf5ea2454dd
Luis Díaz <ldiazn98@gmail.com>
2025-01-22 14:42:25 +0100

    Revert "Add new jwt_secret for ooniprobe service"
    This reverts commit a02600d9e9a62322acb29d0d70e0e1988b42ab6d.

--- tf/environments/dev/main.tf
+++ tf/environments/dev/main.tf
@@ -188,10 +188,6 @@ data "aws_ssm_parameter" "jwt_secret" {
   name = "/oonidevops/secrets/ooni_services/jwt_secret"
 }
 
-data "aws_ssm_parameter" "jwt_secret_ooniprobe" {
-  name = "/oonidevops/secrets/ooni_services/jwt_secret_ooniprobe"
-}
-
 resource "random_password" "prometheus_metrics_password" {
   length  = 32
   special = false
@@ -345,7 +341,7 @@ module "ooniapi_ooniprobe" {
 
   task_secrets = {
     POSTGRESQL_URL              = aws_secretsmanager_secret_version.oonipg_url.arn
-    JWT_ENCRYPTION_KEY          = data.aws_ssm_parameter.jwt_secret_ooniprobe.arn
+    JWT_ENCRYPTION_KEY          = data.aws_ssm_parameter.jwt_secret.arn
     PROMETHEUS_METRICS_PASSWORD = aws_secretsmanager_secret_version.prometheus_metrics_password.arn
   }
 
