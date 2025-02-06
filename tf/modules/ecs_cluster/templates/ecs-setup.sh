6f1b863c00b50f7d48b7f2a48d42f983a65456b6
Luis Díaz <ldiazn98@gmail.com>
2025-02-06 10:56:04 +0100

    Installing node exporter from user data script

--- tf/modules/ecs_cluster/templates/ecs-setup.sh
+++ tf/modules/ecs_cluster/templates/ecs-setup.sh
@@ -7,3 +7,42 @@ ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(ecs_cluster_tags)}
 ECS_ENABLE_TASK_IAM_ROLE=true
 EOF
 
+# Install node exporter on this machine
+DOWNLOAD_LINK='https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz'
+CHECKSUM='6809dd0b3ec45fd6e992c19071d6b5253aed3ead7bf0686885a51d85c6643c66'
+# TODO Checksum this file
+
+# Download node exporter binary
+echo "Downloading node exporter..."
+cd /tmp
+curl -O -L $DOWNLOAD_LINK
+tar xvfz node_exporter-*.*-amd64.tar.gz
+sudo mv node_exporter-*.*-amd64/node_exporter /usr/local/bin/
+
+
+# Add node exporter service user
+echo "Creating node exporter user..."
+sudo useradd -rs /bin/false node_exporter
+
+# Create service file for node exporter
+echo "Setting up service file..."
+cat <<'EOF' >> /tmp/node_exporter.service
+[Unit]
+Description=Node Exporter
+After=network.target
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo mv /tmp/node_exporter.service /etc/systemd/system
+
+# update systemd
+echo "Updating systemd..."
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
