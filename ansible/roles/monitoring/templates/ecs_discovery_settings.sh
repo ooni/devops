35930f40c635b3435e456183f61c6a012c713198
Luis Díaz <ldiazn98@gmail.com>
2025-02-18 10:05:47 +0100

    Add dev variant of secret and access key default variables

--- ansible/roles/monitoring/templates/ecs_discovery_settings.sh
+++ ansible/roles/monitoring/templates/ecs_discovery_settings.sh
@@ -4,5 +4,5 @@
 # to be monitored by Prometheus
 
 export AWS_REGION={{ecs_aws_region}}
-export AWS_SECRET_KEY={{monitoring_secret_key}}
-export AWS_ACCESS_KEY_ID={{monitoring_access_key}}
+export AWS_SECRET_KEY={{monitoring_secret_key_dev}}
+export AWS_ACCESS_KEY_ID={{monitoring_access_key_dev}}
