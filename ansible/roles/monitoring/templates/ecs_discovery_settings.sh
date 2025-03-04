4f6a7eb33be094939a1e827eadd20e38a7bd8559
Luis Díaz <ldiazn98@gmail.com>
2025-03-04 12:56:41 +0100

    Add cronjob for both prod and dev

--- ansible/roles/monitoring/templates/ecs_discovery_settings.sh
+++ ansible/roles/monitoring/templates/ecs_discovery_settings.sh
@@ -4,5 +4,8 @@
 # to be monitored by Prometheus
 
 export AWS_REGION={{ecs_aws_region}}
-export AWS_SECRET_KEY={{monitoring_secret_key_dev}}
-export AWS_ACCESS_KEY_ID={{monitoring_access_key_dev}}
+export AWS_SECRET_KEY_DEV={{monitoring_secret_key_dev}}
+export AWS_ACCESS_KEY_ID_DEV={{monitoring_access_key_dev}}
+
+export AWS_SECRET_KEY_PROD={{monitoring_secret_key_dev}}
+export AWS_ACCESS_KEY_ID_PROD={{monitoring_access_key_dev}}
--- ansible/roles/monitoring/templates/ecs_discovery_wrapper.sh
+++ ansible/roles/monitoring/templates/ecs_discovery_wrapper.sh
@@ -4,5 +4,30 @@
 
 # We read the environment variables from /etc/ooni/ecs_discovery_settings.sh
 
+set -e
+
+# Check if the --env argument is provided
+if [[ "$#" -lt 2 || "$1" != "--env" ]]; then
+    echo "Usage: $0 --env <dev|prod>"
+    exit 1
+fi
+
+# Extract the environment value
+ENV="$2"
+
+# Validate the argument
+if [[ "$ENV" != "dev" && "$ENV" != "prod" ]]; then
+    echo "Error: --env must be either 'dev' or 'prod'"
+    exit 1
+fi
+
+
+# Load env variables from this file
 source /etc/ooni/ecs_discovery_settings.sh
-ecs-discovery.py --output-file {{ecs_targets_file}}
+
+# Execute different commands based on the environment
+if [[ "$ENV" == "dev" ]]; then    
+    ecs-discovery.py --output-file {{ecs_targets_dir}}/dev.json --env dev --secret-key $AWS_SECRET_KEY_DEV --access-key $AWS_ACCESS_KEY_ID_DEV
+elif [[ "$ENV" == "prod" ]]; then
+    ecs-discovery.py --output-file {{ecs_targets_dir}}/prod.json --env prod --secret-key $AWS_SECRET_KEY_PROD --access-key $AWS_ACCESS_KEY_ID_PROD
+fi
