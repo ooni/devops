d27b5557b35004d53b85381f599146348eaa4de6
Luis Díaz <ldiazn98@gmail.com>
2025-03-12 11:30:19 +0100

    Add ** to trigger paths

--- tf/environments/prod/main.tf
+++ tf/environments/prod/main.tf
@@ -300,7 +300,7 @@ module "ooniapi_reverseproxy_deployer" {
   service_name            = "reverseproxy"
   repo                    = "ooni/backend"
   branch_name             = "master"
-  trigger_path            = "ooniapi/services/reverseproxy"
+  trigger_path            = "ooniapi/services/reverseproxy/**"
   buildspec_path          = "ooniapi/services/reverseproxy/buildspec.yml"
   codestar_connection_arn = aws_codestarconnections_connection.oonidevops.arn
 
@@ -459,7 +459,7 @@ module "ooniapi_ooniprobe_deployer" {
   service_name            = "ooniprobe"
   repo                    = "ooni/backend"
   branch_name             = "master"
-  trigger_path            = "ooniapi/services/ooniprobe"
+  trigger_path            = "ooniapi/services/ooniprobe/**"
   buildspec_path          = "ooniapi/services/ooniprobe/buildspec.yml"
   codestar_connection_arn = aws_codestarconnections_connection.oonidevops.arn
 
@@ -511,7 +511,7 @@ module "ooniapi_oonirun_deployer" {
   service_name            = "oonirun"
   repo                    = "ooni/backend"
   branch_name             = "master"
-  trigger_path            = "ooniapi/services/oonirun"
+  trigger_path            = "ooniapi/services/oonirun/**"
   buildspec_path          = "ooniapi/services/oonirun/buildspec.yml"
   codestar_connection_arn = aws_codestarconnections_connection.oonidevops.arn
 
@@ -560,7 +560,7 @@ module "ooniapi_oonifindings_deployer" {
   service_name            = "oonifindings"
   repo                    = "ooni/backend"
   branch_name             = "master"
-  trigger_path            = "ooniapi/services/oonifindings"
+  trigger_path            = "ooniapi/services/oonifindings/**"
   buildspec_path          = "ooniapi/services/oonifindings/buildspec.yml"
   codestar_connection_arn = aws_codestarconnections_connection.oonidevops.arn
 
@@ -608,7 +608,7 @@ module "ooniapi_ooniauth_deployer" {
   service_name            = "ooniauth"
   repo                    = "ooni/backend"
   branch_name             = "master"
-  trigger_path            = "ooniapi/services/ooniauth"
+  trigger_path            = "ooniapi/services/ooniauth/**"
   buildspec_path          = "ooniapi/services/ooniauth/buildspec.yml"
   codestar_connection_arn = aws_codestarconnections_connection.oonidevops.arn
 
