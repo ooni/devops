7a3bb64c31b4424a949b4e993bf18f7a284049aa
Luis Díaz <ldiazn98@gmail.com>
2025-03-31 15:58:23 +0200

    Deactivating broken ecs task monitoring from prod

--- ansible/roles/monitoring/templates/ecs_discovery_wrapper.sh
+++ ansible/roles/monitoring/templates/ecs_discovery_wrapper.sh
@@ -9,4 +9,4 @@
 source /etc/ooni/ecs_discovery_settings.sh
 
 ecs-discovery.py --output-file {{ecs_targets_dir}}/dev.json --env dev --secret-key $AWS_SECRET_KEY_DEV --access-key $AWS_ACCESS_KEY_ID_DEV
-ecs-discovery.py --output-file {{ecs_targets_dir}}/prod.json --env prod --secret-key $AWS_SECRET_KEY_PROD --access-key $AWS_ACCESS_KEY_ID_PROD
+# ecs-discovery.py --output-file {{ecs_targets_dir}}/prod.json --env prod --secret-key $AWS_SECRET_KEY_PROD --access-key $AWS_ACCESS_KEY_ID_PROD
