73c0978a323696cf6438c3e7ebda07ef7de32887
Luis Díaz <ldiazn98@gmail.com>
2025-04-11 13:01:21 +0200

    Add EC2 instance for monitoring proxy

--- tf/environments/dev/main.tf
+++ tf/environments/dev/main.tf
@@ -488,6 +488,66 @@ resource "aws_route53_record" "clickhouse_proxy_alias" {
   ]
 }
 
+#### Monitoring Proxy
+module "ooni_monitoring_proxy" {
+  source = "../../modules/ec2"
+
+  stage = local.environment
+
+  vpc_id              = module.network.vpc_id
+  subnet_id           = module.network.vpc_subnet_public[0].id
+  private_subnet_cidr = module.network.vpc_subnet_private[*].cidr_block
+  dns_zone_ooni_io    = local.dns_zone_ooni_io
+
+  key_name      = module.adm_iam_roles.oonidevops_key_name
+  instance_type = "t3a.nano"
+
+  name = "oonimnprx"
+  ingress_rules = [{
+    from_port   = 9000,
+    to_port     = 9000,
+    protocol    = "tcp",
+    cidr_blocks = module.network.vpc_subnet_private[*].cidr_block,
+    }, {
+    // For the prometheus proxy:
+    from_port   = 9200,
+    to_port     = 9200,
+    protocol    = "tcp"
+    cidr_blocks = [for ip in flatten(data.dns_a_record_set.monitoring_host.*.addrs) : "${tostring(ip)}/32"]
+  }]
+
+  egress_rules = [{
+    from_port   = 0,
+    to_port     = 0,
+    protocol    = "-1",
+    cidr_blocks = ["0.0.0.0/0"],
+    }, {
+    from_port        = 0,
+    to_port          = 0,
+    protocol         = "-1",
+    ipv6_cidr_blocks = ["::/0"]
+  }]
+
+  sg_prefix = "oomnprx"
+  tg_prefix = "mnpr"
+
+  tags = merge(
+    local.tags,
+    { Name = "ooni-tier1-monitoringproxy" }
+  )
+}
+
+resource "aws_route53_record" "monitoring_proxy_alias" {
+  zone_id = local.dns_zone_ooni_io
+  name    = "monitoring.${local.environment}.ooni.io"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = [
+    module.ooni_monitoring_proxy.aws_instance_public_dns
+  ]
+}
+
 #### OONI Run service
 
 module "ooniapi_oonirun_deployer" {
