c96d24dd6075c52f520283955c916d5106c48d92
Luis Díaz <ldiazn98@gmail.com>
2025-04-24 10:48:18 +0200

    Ignore changes to user data on ec2 machines for redeploy

--- tf/modules/ec2/main.tf
+++ tf/modules/ec2/main.tf
@@ -87,6 +87,7 @@ resource "aws_instance" "ooni_ec2" {
 
   lifecycle {
     create_before_destroy = true
+    ignore_changes = [ user_data ]
   }
 
   tags = merge(var.tags, {MonitoringActive = var.monitoring_active})
