7806d93d99f0ddb21542e8f7d972f8867f91d60f
Luis Díaz <ldiazn98@gmail.com>
2025-07-02 10:52:02 +0200

    add instance public ip to outputs variables

--- tf/environments/dev/main.tf
+++ tf/environments/dev/main.tf
@@ -448,7 +448,7 @@ module "ooni_clickhouse_proxy" {
     from_port   = 9000,
     to_port     = 9000,
     protocol    = "tcp",
-    cidr_blocks = module.network.vpc_subnet_private[*].cidr_block,
+    cidr_blocks = concat(module.network.vpc_subnet_private[*].cidr_block, [format("%s/32", module.ooni_fastpath.aws_instance_public_ip)]),
     }, {
     // For the prometheus proxy:
     from_port   = 9200,
--- tf/modules/ec2/outputs.tf
+++ tf/modules/ec2/outputs.tf
@@ -13,3 +13,7 @@ output "ec2_sg_id" {
 output "aws_instance_private_ip" {
   value = aws_instance.ooni_ec2.private_ip
 }
+
+output "aws_instance_public_ip" {
+  value = aws_instance.ooni_ec2.public_ip
+}
