ac8abb729b004379cc80392711b82d96d74c3303
Luis Díaz <ldiazn98@gmail.com>
2025-09-17 10:27:03 +0200

    Change to branch with geoip logs

--- tf/environments/dev/main.tf
+++ tf/environments/dev/main.tf
@@ -342,7 +342,7 @@ module "ooniapi_ooniprobe_deployer" {
 
   service_name            = "ooniprobe"
   repo                    = "ooni/backend"
-  branch_name             = "master"
+  branch_name             = "investigate-geoip-reporting"
   trigger_path            = "ooniapi/services/ooniprobe/**"
   buildspec_path          = "ooniapi/services/ooniprobe/buildspec.yml"
   codestar_connection_arn = aws_codestarconnections_connection.oonidevops.arn
