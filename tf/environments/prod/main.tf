583de7e93961935c84b90d52224c25b20d8e2bb7
Luis Díaz <ldiazn98@gmail.com>
2025-10-02 16:19:19 +0200

    test measurement fixes in dev

--- tf/environments/prod/main.tf
+++ tf/environments/prod/main.tf
@@ -877,7 +877,7 @@ module "ooniapi_oonimeasurements_deployer" {
 
   service_name            = "oonimeasurements"
   repo                    = "ooni/backend"
-  branch_name             = "master"
+  branch_name             = "fix/measurements-timestamp-bug"
   trigger_path            = "ooniapi/services/oonimeasurements/**"
   buildspec_path          = "ooniapi/services/oonimeasurements/buildspec.yml"
   codestar_connection_arn = aws_codestarconnections_connection.oonidevops.arn
