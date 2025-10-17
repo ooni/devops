6202666f57842ef1f6dd9683dce498e212e753b1
Luis Díaz <ldiazn98@gmail.com>
2025-10-17 13:54:09 +0200

    enable rebalancing through availability zones

--- tf/modules/ooniapi_service/main.tf
+++ tf/modules/ooniapi_service/main.tf
@@ -103,6 +103,8 @@ resource "aws_ecs_service" "ooniapi_service" {
   deployment_minimum_healthy_percent = 50
   deployment_maximum_percent         = 200
 
+  availability_zone_rebalancing = "ENABLED"
+
   ordered_placement_strategy {
     type  = "spread"
     field = "attribute:ecs.availability-zone"
