9a63463586fbd1e5a3c80e580496bd030f145f5d
Luis Díaz <ldiazn98@gmail.com>
2025-11-12 12:34:15 +0100

    Set branch of ooniprobe to test config file read

--- tf/environments/dev/main.tf
+++ tf/environments/dev/main.tf
@@ -382,7 +382,7 @@ module "ooniapi_ooniprobe_deployer" {
 
   service_name            = "ooniprobe"
   repo                    = "ooni/backend"
-  branch_name             = "investigate-geoip-reporting"
+  branch_name             = "s3-config-dependency"
   trigger_path            = "ooniapi/services/ooniprobe/**"
   buildspec_path          = "ooniapi/services/ooniprobe/buildspec.yml"
   codestar_connection_arn = aws_codestarconnections_connection.oonidevops.arn
