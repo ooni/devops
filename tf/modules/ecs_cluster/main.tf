e10692b7aaeeca2111847aabb7c7e36dd87b99dc
Luis Díaz <ldiazn98@gmail.com>
2025-11-21 11:58:49 +0100

    adjusting target capacity

--- tf/modules/ecs_cluster/main.tf
+++ tf/modules/ecs_cluster/main.tf
@@ -236,7 +236,7 @@ resource "aws_ecs_capacity_provider" "capacity_provider" {
         maximum_scaling_step_size = 1000
         minimum_scaling_step_size = 1
         status                    = "ENABLED"
-        target_capacity           = 100
+        target_capacity           = 90
       }
     }
 }
