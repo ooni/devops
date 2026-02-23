f4987f46c4d1a917faaf2508bc1a8d1677edfb1e
Luis Díaz <ldiazn98@gmail.com>
2026-02-23 11:32:58 +0100

    Point dev version of ooniprobe to synced version of userauth

--- tf/environments/dev/main.tf
+++ tf/environments/dev/main.tf
@@ -554,7 +554,7 @@ module "ooniapi_ooniprobe_deployer" {
 
   service_name            = "ooniprobe"
   repo                    = "ooni/backend"
-  branch_name             = "patch-sign-credential-bug"
+  branch_name             = "test-userauth"
   trigger_path            = "ooniapi/services/ooniprobe/**"
   buildspec_path          = "ooniapi/services/ooniprobe/buildspec.yml"
   codestar_connection_arn = aws_codestarconnections_connection.oonidevops.arn
