1d3412acf0bf19a365d6119f89217c92381baeae
Luis Díaz <ldiazn98@gmail.com>
2026-05-19 13:04:45 +0200

    Point ooniprobe in dev to master

--- tf/environments/dev/main.tf
+++ tf/environments/dev/main.tf
@@ -559,7 +559,7 @@ module "ooniapi_ooniprobe_deployer" {
 
   service_name            = "ooniprobe"
   repo                    = "ooni/backend"
-  branch_name             = "remove-report-id"
+  branch_name             = "master"
   environment             = local.environment
   trigger_path            = "ooniapi/services/ooniprobe/**"
   buildspec_path          = "ooniapi/services/ooniprobe/buildspec.yml"
