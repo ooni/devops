e8e191e1c60b1ca137231b187aa36c9eef139e8b
Aaron Gibson <aagbsn@extc.org>
2026-06-09 14:18:25 +0200

    add scheduled_service module

---
+++ tf/modules/scheduled_service/variables.tf
@@ -0,0 +1,79 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "eu-central-1"
+}
+
+variable "first_run" {
+  default = false
+}
+
+variable "key_name" {
+  description = "Name of AWS key pair"
+}
+
+variable "service_name" {
+  description = "short service name. will become the first part of the fqdn eg. <service_name>.prod.ooni.io"
+}
+
+variable "stage" {
+  default = "one of dev, stage, test, prod"
+}
+
+variable "vpc_id" {
+  description = "the id of the VPC to deploy the instance into"
+}
+
+variable "tags" {
+  description = "tags to apply to the resources"
+  default     = {}
+  type        = map(string)
+}
+
+variable "task_memory" {
+  default     = 64
+  description = "https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size"
+}
+
+variable "memory_hard_limit" {
+  default     = 1024
+  description = "https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size"
+}
+
+variable "dns_zone_ooni_io" {
+  description = "id of the DNS zone for ooni_io"
+}
+
+variable "default_docker_image_url" {
+  description = "the url to the default docker image unless there is one already defined in the task definition"
+}
+
+variable "ecs_cluster_id" {
+  description = "id of the cluster to deploy into"
+}
+
+variable "task_secrets" {
+  default = {}
+  type    = map(string)
+}
+
+variable "task_environment" {
+  default = {}
+  type    = map(string)
+}
+
+variable "ooniapi_service_security_groups" {
+  description = "the shared web security group from the ecs cluster"
+  type        = list(string)
+}
+
+variable "schedule_expression" {
+  type    = string
+  default = "cron(0 6 ? * MON-FRI *)" # example default; callers override
+}
+
+variable "scheduled_task_cluster" {
+  type    = string
+  description = "Name of the ECS cluster to run the scheduled task on."
+  nullable = true
+  default = null
+}
