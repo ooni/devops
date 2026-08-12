42cafb5418f523c87ab5cd3ece3d591bfaff1753
Luis Díaz <ldiazn98@gmail.com>
2026-08-12 09:39:57 +0200

    point oonimeasurements to master

--- tf/environments/dev/main.tf
+++ tf/environments/dev/main.tf
@@ -1117,7 +1117,7 @@ module "ooniapi_oonimeasurements_deployer" {
 
   service_name            = "oonimeasurements"
   repo                    = "ooni/backend"
-  branch_name             = "labeling"
+  branch_name             = "master"
   environment             = local.environment
   trigger_path            = "ooniapi/services/oonimeasurements/**"
   buildspec_path          = "ooniapi/services/oonimeasurements/buildspec.yml"
